819f43169cf7a113e65a616cf233547ef1f9bb16
Fabio Salvagno <fabiosalvagno@yahoo.co.jp>
2025-11-22 23:12:29 +0900

    sempre index di attivita

--- resources/views/attivita/index.blade.php
+++ resources/views/attivita/index.blade.php
@@ -56,11 +56,12 @@
                                     @endif
                                 </td>
 
-                                <td class="px-6 py-4 max-w-xs whitespace-normal break-words" id="tags-container-{{ $attivita->id }}">
-                                    <div class="tags-display flex justify-between items-center group">
+                                <td class="px-6 py-4 align-top" id="tags-container-{{ $attivita->id }}">
+                                    <div class="tags-display flex flex-wrap gap-2 justify-start items-center group">
                                         <div class="tags-content">
                                             {!! \App\Helpers\TagHelper::generateTagsHTML($attivita->tags) !!}
                                         </div>
+
                                         <button class="edit-tags-btn invisible group-hover:visible p-1 text-gray-400 hover:text-blue-600" data-id="{{ $attivita->id }}">
                                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
@@ -68,7 +69,7 @@
                                         </button>
                                     </div>
 
-                                    <div class="tags-edit hidden">
+                                    <div class="tags-edit hidden mt-2">
                                         <input type="text" class="tags-input w-full text-sm border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:ring-blue-500" value="{{ $attivita->tags }}">
                                         <div class="mt-2 flex space-x-2 text-xs">
                                             <button class="save-tags-btn bg-green-600 text-white px-2 py-1 rounded hover:bg-green-700" data-id="{{ $attivita->id }}">Salva</button>
